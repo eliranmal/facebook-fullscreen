aa7332f5f20d30796449e20b5c1aa3eaafdc071e
eliranmal <eliranmal@gmail.com>
2016-02-08 00:42:25 +0200

    - tie event to extension action button - lose dependency on doc/window load - refactor

--- app/content-script.js
+++ app/content-script.js
@@ -2,167 +2,100 @@
 
 // todo - publish to chrome web store :)
 
-(function(win, doc) {
 
-    // init constants
+// init constants
 
-    var selectors = {
-        log: '#log',
+var win = window,
+    doc = document,
+    selectors = {
         iframeCanvas: '#iframe_canvas'
-    };
-
-    var prefixes = [
+    },
+    prefixes = [
         'webkit',
         'moz',
         'ms',
         'o'
     ];
 
-    // declare functions
+// declare functions
 
-    var log = function() {
-        var i, arg,
-            args = [].slice.call(arguments);
-        if (logElement) {
-            logElement.innerHTML += args.join(' ') + '<br />';
-        } else {
-            console.log.apply(console, arguments);
-        }
-    };
+var log = function() {
+    console.log.apply(console, arguments);
+};
 
-    var setVendorAttribute = function(element, attribute, value) {
-        var i,
-            val = (value + '').toLowerCase();
-        for (i = 0; i < prefixes.length; i++) {
-            element.setAttribute(prefixes[i] + attribute, val);
-        }
-        element.setAttribute(attribute, val);
-    }
+var err = function() {
+    console.error.apply(console, arguments);
+};
 
-    var getRequestFullScreenImpl = function(element) {
-        var propName, suffix = 'requestfullscreen';
-        for (propName in element) {
-            if (propName.toLowerCase().endsWith(suffix)) {
-                return propName;
-            }
-        }
-    }
-
-    var elementRequestFullScreen = function(selector) {
-        var element = doc.querySelector(selector);
-        log('requestfullscreen implementation',
-            getRequestFullScreenImpl(element));
-        return function(e) {
-            log('requesting fullscreen on', element, selector);
-            try {
-                if (element.requestFullscreen) {
-                    element.requestFullscreen();
-                } else if (element.msRequestFullscreen) {
-                    element.msRequestFullscreen();
-                } else if (element.mozRequestFullScreen) {
-                    element.mozRequestFullScreen();
-                } else if (element.webkitRequestFullscreen) {
-                    element.webkitRequestFullscreen();
-                }
-            } catch (ex) {
-                log('[error]', ex);
-            }
-        }
-    };
-
-
-    // create elements
-
-    var iFrameCanvasElement = (function() {
-        var el = doc.querySelector(selectors.iframeCanvas);
-        if (!el) {
-            return;
-        }
-        setVendorAttribute(el, 'allowFullScreen', 'true');
-        return el;
-    })();
-
-    var logElement = (function() {
-        var el = doc.querySelector(selectors.log);
-        if (!el) {
-            return;
-        }
-        el.style.lineHeight = '1.5';
-        el.style.padding = '1em';
-        el.style.backgroundColor = '#eee';
-        return el;
-    })();
-
-
-    var fullScreenButton = (function() {
-        var b = doc.createElement('button');
-        b.addEventListener('click', elementRequestFullScreen(
-            selectors.iframeCanvas));
-        b.textContent = '[ fullscreen ]';
-        b.style.position = 'fixed';
-        b.style.bottom = 0;
-        b.style.left = 0;
-        b.style.padding = '.4em .8em';
-        b.style.fontSize = '1.2em';
-        //  b.style.fontFamily = '"trebuchet ms", sans-serif';
-        b.style.fontFamily = '"courier new", monospace';
-        //  b.style.fontWeight = 'bold';
-        b.style.zIndex = '99999';
-        return b;
-    })();
-
-
-
-    // go
-
-    doc.getElementsByTagName('body')[0].appendChild(fullScreenButton);
-
-
-
-    /*
-  var setVendorStyle = function(element, property, value) {
+var setVendorAttribute = function(element, attribute, value) {
     var i,
-      prop = property + '',
-      cProp = prop.slice(0, 1).toUpperCase() + prop.slice(1);
+        val = (value + '').toLowerCase();
     for (i = 0; i < prefixes.length; i++) {
-	    element.style[prefixes[i] + cProp] = value;
+        element.setAttribute(prefixes[i] + attribute, val);
     }
-    element.style[prop] = value;
-  }
+    element.setAttribute(attribute, val);
+}
 
-var zoomIn = function(selector) {
-    var el = doc.querySelector(selector);
-    log('zooming in on', el);
-    el.style.zoom = 2;
-    setVendorStyle(el, 'transform', 'scale(2)');
-    setVendorStyle(el, 'transformOrigin', '0 0');
-    log('zoomed in on', el);
-  };
-
-  var zoomOut = function(selector) {
-    var el = doc.querySelector(selector);
-    log('zooming out from', el);
-    el.style.zoom = 1;
-    setVendorStyle(el, 'transform', 'scale(1)');
-    setVendorStyle(el, 'transformOrigin', '0 0');
-    log('zoomed out from', el);
-  };
-
-  doc.addEventListener('fullscreenchange', function(event) {
-    if (doc.fullscreenEnabled) {
-      doc.fullscreenElement;
-      zoomIn('body');
-    } else {
-      zoomOut('body');
+var getRequestFullScreenImpl = function(element) {
+    var propName, suffix = 'requestfullscreen';
+    for (propName in element) {
+        if (propName.toLowerCase().endsWith(suffix)) {
+            return propName;
+        }
     }
-  });
+}
+
+var requestFullscreen = function() {
+    var element = this.element;
+    log('requesting fullscreen', element);
+    try {
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        }
+    } catch (ex) {
+        err(ex);
+    }
+};
+
+var elementRequestFullScreen = function(selector) {
+    var element = doc.querySelector(selector);
+    if (!element) {
+        err('failed to find element', selector);
+        return;
+    }
+    log('requestfullscreen implementation', getRequestFullScreenImpl(element));
+    return requestFullscreen.bind({
+        element: element
+    });
+};
 
 
-  // a dirty hack for jsfiddle, might be good for other envs, too
-  if (win.frameElement) {
-    elementAllowFullScreen(win.frameElement);
-  }
-  log('win.frameElement is ', win.frameElement);
-  */
+// bootstrap
 
-})(window, document);
+var bootstrap = function() {
+    log('bootstrapping');
+    var el = doc.querySelector(selectors.iframeCanvas);
+    if (el) {
+        log('setting "allowFullScreen" attribute to "true"', el);
+        setVendorAttribute(el, 'allowFullScreen', 'true');
+    }
+};
+
+var goFullscreen = function() {
+    elementRequestFullScreen(selectors.iframeCanvas)();
+};
+
+
+bootstrap();
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message === 'fullscreen:go') {
+        goFullscreen();
+    }
+});
